842b11291c296aee4bac69395f6db8cb4d77510f
Shigeki Suwa <suwa.shigeki@gmail.com>
2016-08-30 10:43:42 +0900

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(req, resp) {
+  resp.writeHead(200, {'Content-Type': 'text/plain'});
+  resp.write('hello!');
+  resp.end();
+}).listen(80);
