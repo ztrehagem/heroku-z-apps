73a29263a0bda88981029178a79eee2fb2aa2b53
Shigeki Suwa <suwa.shigeki@gmail.com>
2016-09-03 18:44:35 +0900

    catch exception

--- server/utils/response-utils.js
+++ server/utils/response-utils.js
@@ -1,6 +1,13 @@
 exports.writeNotFound = function() {
-  console.log('not found');
+  console.log('respond: Not Found');
   this.writeHead(404, {'Content-Type': 'text/plain'});
   this.write('not found');
+  this.end();
+};
+
+exports.writeInternalServerError = function() {
+  console.log('respond: Internal Server Error');
+  this.writeHead(500, {'Content-Type': 'text/plain'});
+  this.write('internal server error');
   this.end();
 };
