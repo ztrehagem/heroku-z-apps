b1272967a79812e8231ae435028b62069e709265
Shigeki Suwa <suwa.shigeki@gmail.com>
2016-09-03 17:51:20 +0900

    update z-router

--- server/handler.js
+++ server/handler.js
@@ -2,8 +2,10 @@ module.exports = handler;
 
 var URL = require('url');
 var PATH = require('path');
-var router = require('z-router')(require('./routes'));
-var fileServer = new (require('node-static').Server)('./public');
+var router = require('z-router')(require('./routes'), {
+  ctrlDir: 'server/controllers'
+});
+var fileServer = new (require('node-static').Server)('public');
 var responseUtils = require('./utils/response-utils');
 
 console.log(router.routesToString());
@@ -14,29 +16,18 @@ function handler(req, resp) {
 
   console.log('requested:', req.url);
 
-  if( !route(req, resp) ) return resp.writeNotFound();
-}
-
-function route(req, resp) {
   var pathname = PATH.resolve(URL.parse(req.url).pathname);
-  if( !pathname.startsWith('/api/') ) {
-    if( !PATH.extname(pathname).length ) {
-      fileServer.serveFile('/index.html', 200, {}, req, resp);
-    } else {
-      fileServer.serve(req, resp, function(e, res) {
-        if( e && e.status == 404 ) {
-          resp.writeNotFound();
-        }
-      });
-    }
-    return true;
+
+  if( pathname.startsWith('/api/') ) {
+    var route = router.route(req.method, pathname);
+    if( route && typeof route.controller == 'function' )
+      return route.controller(req, resp, route.params);
+    return resp.writeNotFound();
+  } else if( !PATH.extname(pathname).length ) {
+    fileServer.serveFile('/index.html', 200, {}, req, resp);
+  } else {
+    fileServer.serve(req, resp, function(e, res) {
+      if( e && e.status == 404 ) resp.writeNotFound();
+    });
   }
-  var route = router.route(req.method, pathname);
-  if( !route ) return;
-  var ctrlModule = require('./controllers' + route.ctrlPath);
-  if( !ctrlModule ) { console.warn('undefined controller module'); return; }
-  var ctrl = ctrlModule[route.actionName];
-  if( typeof ctrl != 'function' ) { console.warn('undefined action method'); return; }
-  ctrl(req, resp, route.params);
-  return true;
 }
