641e1f32d95b50274b26e46603c01013de987e7a
Shigeki Suwa <suwa.shigeki@gmail.com>
2017-05-31 11:51:06 +0900

    localでgulpしたときにサーバー上のlibにリンクする（NODE_ENVに基づく）

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ const Zelixir = require('z-elixir');
 const Resource = Zelixir.Resource;
 const Task = Zelixir.Task;
 const config = Zelixir.config;
+const $ = Zelixir.modules;
 
 config.enabledNgAnnotate = true;
 config.enabledBabel = true;
 
 const JS_APP_NAMES = ['root', 'shift', 'geister'];
 
-Resource('html', Resource.template('html'));
+Resource('html', Resource.template('html')
+  .option({replace: {
+    angular: {
+      local: '/js/lib/angular.min.js',
+      cdn: '//ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular.min.js'
+    },
+    'angular-ui-router': {
+      local: '/js/lib/angular-ui-router.min.js',
+      cdn: '//npmcdn.com/angular-ui-router/release/angular-ui-router.min.js'
+    }
+  }})
+);
 Resource('sass', Resource.template('sass'));
 JS_APP_NAMES.forEach((name)=> {
   Resource(`js-${name}`, Resource.create()
@@ -19,7 +31,17 @@ JS_APP_NAMES.forEach((name)=> {
   );
 });
 
-Task('html', ['html'], Task.template('html'));
+// Task('html', ['html'], Task.template('html'));
+Task('html', ['html'], (res)=>
+  $.gulp.src(res.src)
+    .pipe($.errorHandler())
+    .pipe($.changed(res.dest))
+    .pipe($.replace(/{\$(.+?)}/g, (match, name)=>
+      res.replace[name][process.env.NODE_ENV ? 'cdn' : 'local']
+    ))
+    .pipe($.html({collapseWhitespace: true, minifyCSS: true, minifyJS: true}))
+    .pipe($.gulp.dest(res.dest))
+);
 Task('sass', ['sass'], Task.template('sass'));
 // Task('js', ['js'], Task.template('js'));
 JS_APP_NAMES.forEach((name)=> {
